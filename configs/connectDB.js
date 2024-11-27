7b0ad45cfbfef706defd56a6021af00179401dad
LoeschaiTH <your-email@example.com>
2024-11-27 23:59:46 +0700

    test[config]--connect database

--- configs/connectDB.js
+++ configs/connectDB.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { log } = require('node:console');
 require('dotenv').config();
 const mongoDbSet = process.env.MONGODB_URI;
+console.log(mongoDbSet);
 
 
 const connectDB = async () => {
