73091d9c2fc037de39453674edcf656e6914f310
LoeschaiTH <your-email@example.com>
2024-11-28 00:22:41 +0700

    test

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,14 +2,20 @@ const product = require('../models/products');
 const { formatNumber } = require('../utils/formatNumber');
 
 exports.getProducts = async (req, res) => {
-  const products = await product.find({});
-    try {
-      res.render('products/index', { title: 'products', message: 'Products management',products: products,
-      formatNumber: formatNumber});
-    } catch (err) {
-      res.status(500).send('Error fetching data');
-    }
-  };
+  try {
+    const products = await product.find({});
+    res.render('products/index', { 
+      title: 'Products', 
+      message: 'Products management', 
+      products: products,
+      formatNumber: formatNumber
+    });
+  } catch (err) {
+    // หากเกิดข้อผิดพลาดในการดึงข้อมูล
+    console.error('Error fetching data:', err);
+    res.status(500).send('Error fetching data');  // ส่งข้อผิดพลาดกลับไปที่ผู้ใช้
+  }
+};
 
   exports.addProduct = async (req, res) => {
     try {
